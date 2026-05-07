14751d3cbf22c56fcf3b7715744889b29e2d6f67
pavann2005 <pavannagaraj9788@gmail.com>
2026-05-07 16:10:56 +0530

    Create mockRestaurantData.js

---
+++ src/mockRestaurantData.js
@@ -0,0 +1,39 @@
+const mockRestaurantData = [
+  {
+    info: {
+      id: "1",
+      name: "Pizza Hut",
+      cuisines: ["Pizza", "Fast Food"],
+      avgRating: "4.2",
+      costForTwo: "₹300 FOR TWO",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/17/0dc3eb1b-0145-4af6-95d5-f9a47f8d7851_1003413.jpg",
+    },
+  },
+
+  {
+    info: {
+      id: "2",
+      name: "KFC",
+      cuisines: ["Burger", "Chicken"],
+      avgRating: "4.1",
+      costForTwo: "₹400 FOR TWO",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/ed6b91e6-c4d3-42e9-83c0-fc2c08d6ddf9_855356.JPG",
+    },
+  },
+
+  {
+    info: {
+      id: "3",
+      name: "Domino's",
+      cuisines: ["Pizza"],
+      avgRating: "4.3",
+      costForTwo: "₹350 FOR TWO",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/13/44d5ab7f-3c1d-40f8-8944-0f6b1f12e3a3_995723.JPG",
+    },
+  },
+];
+
+export default mockRestaurantData;
